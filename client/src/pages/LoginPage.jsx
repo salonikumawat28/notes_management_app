342e1349f3a01235e4e7ea44f021b5f2c8fd7902
saloni kumawat <545saloni@gmail.com>
2023-10-15 14:48:13 +0200

    Dummy login page in React

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import Login from "../components/Login";
+import Welcome from "../components/Welcome";
+
+function LoginPage() {
+    return (
+        <div>
+            <Welcome />
+            <Login />
+        </div>
+    );
+}
+
+export default LoginPage;
