1e123f27638921e997deed39e65a07dbf682c1e4
saloni kumawat <545saloni@gmail.com>
2023-10-15 18:42:05 +0200

    React context for authentication state

---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,16 @@
+import { useContext, useState, createContext } from "react";
+
+const AuthContext = createContext();
+
+export function AuthProvider({children}) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    return (
+        <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn}}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuthContext() {
+    return useContext(AuthContext);
+}
