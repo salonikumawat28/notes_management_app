90d3b783a661b4431dca250a628f10b4ba74d52f
saloni kumawat <545saloni@gmail.com>
2023-10-15 20:45:09 +0200

    Storage listener for login state

--- client/src/NotesManagementApp.jsx
+++ client/src/NotesManagementApp.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import { useAuthContext } from './contexts/AuthContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
 function NotesMangementApp() {
-  const {isLoggedIn} = useAuthContext();
+    const {isLoggedIn, setIsLoggedIn} = useAuthContext();
+
+    function handleStorageChange(event) {
+        if (event.key === 'isLoggedIn') {
+            setIsLoggedIn(!!localStorage.getItem('isLoggedIn'));
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('storage', handleStorageChange);
+
+        return () => window.removeEventListener('storage', handleStorageChange);
+    }, []);
+
+  
 
   return (
     <div>
