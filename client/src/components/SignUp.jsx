101fe593ed7cec5d990ad301582d208f3b792600
saloni kumawat <545saloni@gmail.com>
2023-10-19 17:37:59 +0200

    axios and prevent default for signup

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,55 +1,91 @@
-import { useState } from 'react';
-import '../css/SignUp.css';
+import { useState } from "react";
+import "../css/SignUp.css";
+import axios from "axios";
 
 function SignUp() {
-    const [user, setUser] = useState("");
+  const [newUser, setNewUser] = useState({ name: "", email: "", password: "" });
 
-    const requestInfo = {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(user)
-    };
+  async function signUpUser(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    // const requestInfo = {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify(newUser)
+    // };
 
-     async function signUpUser() {
-        const response = await fetch("http://localhost:9000/users/", requestInfo);
-        const signedUpUser = await JSON.stringify(response);
-        setUser(signedUpUser);
+    try {
+      const response = await axios.post(
+        "http://localhost:9000/users/",
+        newUser,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      // const response = await fetch("http://localhost:9000/users/", requestInfo);
+      //   console.log("response daya is: ", response.data);
+      // const signedUpUser = await response.json();
+      const signedUpUser = response.data;
+      console.log("Signedup user: ", signedUpUser);
+      setNewUser(signedUpUser);
+    } catch (error) {
+      console.error("Error:", error.message);
     }
+  }
 
-    function onNameChange(event) {
-        setUser({...user, name: event.target.value});
-    }
+  function onNameChange(event) {
+    setNewUser({ ...newUser, name: event.target.value });
+  }
 
-    function onEmailChange(event) {
-        setUser({...user, email: event.target.value});
-    }
+  function onEmailChange(event) {
+    setNewUser({ ...newUser, email: event.target.value });
+  }
 
-    function onPasswordChange(event) {
-        setUser({...user, password: event.target.value});
-    }
+  function onPasswordChange(event) {
+    setNewUser({ ...newUser, password: event.target.value });
+  }
 
-    return (
-        <div>
-            <form onSubmit={signUpUser}>
-                <label> Name: </label>
-                <input type="text" name="name"  value={user.name} onChange={onNameChange} required/>
-                
-                <label> Email: </label>
-                <input type="email" name="email" value={user.email} onChange={onEmailChange} required/>
-                
-                <label> Password: </label>
-                <input type="password" name="password" value={user.password} onChange={onPasswordChange} required />
+  return (
+    <div>
+      <form onSubmit={signUpUser}>
+        <label> Name: </label>
+        <input
+          type="text"
+          name="name"
+          value={newUser.name}
+          onChange={onNameChange}
+          required
+        />
 
-                <div className='Link'>
-                    <a href="#">Already have an account?</a>
-                </div>
-                
-                <button type="submit">Sign Up</button>
-            </form>
+        <label> Email: </label>
+        <input
+          type="email"
+          name="email"
+          value={newUser.email}
+          onChange={onEmailChange}
+          required
+        />
+
+        <label> Password: </label>
+        <input
+          type="password"
+          name="password"
+          value={newUser.password}
+          onChange={onPasswordChange}
+          required
+        />
+
+        <div className="Link">
+          <a href="#">Already have an account?</a>
         </div>
-    );
+
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
