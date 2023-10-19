ae86ab12ae3efd96844ac3f08d71f950b8965fa3
saloni kumawat <545saloni@gmail.com>
2023-10-19 17:39:37 +0200

    login method in user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const AutoIncrementFactory = require("mongoose-sequence");
-
 const AutoIncrement = AutoIncrementFactory(mongoose);
+const _ = require('underscore');
 
 // Note: Setting _id to false so that mongoose doesn't auto create the _id.
 const userSchema = new mongoose.Schema(
@@ -35,8 +35,25 @@ const userSchema = new mongoose.Schema(
   },{ _id: false }
 );
 
+// Auto increment.
 // Note: Setting mongoose-sequence to auto increment the _id.
 userSchema.plugin(AutoIncrement, { id: "user_id_counter", inc_field: "_id" });
+
+// Custom methods for User model
+// TODO: User password encryption
+userSchema.statics.login = async function ({email, password}) {
+  console.log("userModel.login started");
+  const user = await this.findOne({email: email});
+  console.log("userModel.login found user: ", user);
+  if(_.isEmpty(user)) return null;
+  console.log("user is not null ");
+  if(user.password === password) {
+    console.log("user password matched ");
+    return user;
+  }
+  console.log("user password didnt match ");
+  return null;
+};
 
 const userModel = mongoose.model("User", userSchema);
 
