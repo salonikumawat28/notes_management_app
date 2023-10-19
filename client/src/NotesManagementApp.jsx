6485a89a592517be9c23fd824bd3b03bc785bff8
saloni kumawat <545saloni@gmail.com>
2023-10-19 17:58:00 +0200

    refactoring notes management app compoentn

--- client/src/NotesManagementApp.jsx
+++ client/src/NotesManagementApp.jsx
@@ -1,24 +1,25 @@
-import { useEffect } from 'react';
-import { useAuthContext } from './contexts/AuthContext';
-import AuthHomePage from './pages/AuthHomePage';
-import PublicHomePage from './pages/PublicHomePage';
-import './css/NotesManagementApp.css';
-import _ from 'underscore';
+import { useEffect } from "react";
+import { useAuthContext } from "./contexts/AuthContext";
+import AuthHomePage from "./pages/AuthHomePage";
+import PublicHomePage from "./pages/PublicHomePage";
+import "./css/NotesManagementApp.css";
+import _ from "underscore";
 
 function NotesMangementApp() {
-    const {isLoggedIn, setUser} = useAuthContext();
+  const { isLoggedIn, setUser } = useAuthContext();
 
+  // To handle cross-tab login/logout changes.
+  useEffect(() => {
     function handleStorageChange(event) {
-        if (event.key === 'user') {
-            setUser(!!localStorage.getItem('user'));
-        }
+      if (event.key === "user") {
+        setUser(!!localStorage.getItem("user"));
+      }
     }
 
-    useEffect(() => {
-        window.addEventListener('storage', handleStorageChange);
+    window.addEventListener("storage", handleStorageChange);
 
-        return () => window.removeEventListener('storage', handleStorageChange);
-    }, []);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
 
   return (
     <div className="Box">
